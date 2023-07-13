6a459ce6e14e7d6c5a186f5c65ab796edea9ff42
Alessio Satta <alessiosatta@outlook.it>
2023-07-13 12:54:54 +0200

    update: rollup.config and package.json

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,21 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import del from 'rollup-plugin-delete'
 
-export default {
+export default [{
   input:"src/models/index.ts",
-
   output: {
     dir: "output",
     entryFileNames: 'brello-business-logic.js',
-    format: "es",
+    format: 'es',
   },
   plugins: [typescript(), del({ targets: 'output/*' })],
-};
+},
+{
+  input: `src/models/index.ts`,
+  plugins: [dts()],
+  output: {
+    file: `output/brello-business-logic.d.ts`,
+    format: 'es',
+},
+}
+]
--- src/models/index.ts
+++ src/models/index.ts
@@ -2,5 +2,4 @@ export * from "./app";
 export * from "./board";
 export * from "./column";
 export * from "./task";
-export * from "../interfaces/index";
 export * from "../dataProvider/index";
